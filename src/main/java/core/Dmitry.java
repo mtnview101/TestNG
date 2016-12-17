2aaa01f78813a65cc517ae22a8316d4a5a0a12c9
mtnview101 <mtnview101@gmail.com>
2016-12-16 21:46:35 -0800

    second commit

---
+++ src/main/java/core/Dmitry.java
@@ -0,0 +1,10 @@
+package core;
+
+
+public class Dmitry 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Dmitry is the best in the World!" );
+    }
+}
---
+++ src/test/java/core/NewGenerationTest.java
@@ -0,0 +1,11 @@
+package core;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+public class NewGenerationTest {
+  @Test
+  public void f() {
+	  String s="TestNG";
+	  assertEquals("TestNG",s);
+  }
+}
