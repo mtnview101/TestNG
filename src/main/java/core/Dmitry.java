3337b15b83f27d59dcfe83819456c6b6ef3efb0c
mtnview101 <mtnview101@gmail.com>
2017-03-17 19:49:58 -0700

    commit 3

--- src/main/java/core/Dmitry.java
+++ src/main/java/core/Dmitry.java
@@ -3,6 +3,10 @@ package core;
 
 public class Dmitry 
 {
+	public static String alex (String string){
+		String a="TestNG";
+		return(a);
+	}
     public static void main( String[] args )
     {
         System.out.println( "Dmitry is the best in the World!" );
--- src/test/java/core/NewGenerationTest.java
+++ src/test/java/core/NewGenerationTest.java
@@ -3,9 +3,23 @@ package core;
 import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 public class NewGenerationTest {
-  @Test
-  public void f() {
-	  String s="TestNG";
-	  assertEquals("TestNG",s);
-  }
+	  @Test
+	  public void f1() {
+		  String s=Dmitry.alex("dima");
+		  assertEquals("TestNG",s);}
+	  
+	  @Test
+	  public void f2() {
+		  String s="TestNG";
+		  assertEquals("TestNG",s);}
+		  
+	  @Test
+	  public void f3() {
+		  String s="TestNG";
+		  assertEquals("TestNG",s);}
+	  @Test
+	  public void f4() {
+		  String s="TestNG";
+		  assertEquals("TestNG",s);}
+  
 }
